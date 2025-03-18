7e077017d5e50ba248c8fb2854298fd01b54cb0d
Oleksii <alexey.shevchenko.03@gmail.com>
2025-03-18 12:33:08 +0200

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,43 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './not-found/page-not-found/page-not-found.component';
+import { RegComponent } from './reg/reg/reg.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'reg', component: RegComponent },
+  {
+    path: 'auth',
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./auth/auth/auth.component').then((m) => m.AuthComponent),
+      },
+      {
+        path: 'forget-password',
+        loadComponent: () =>
+          import('./auth/forget-password/forget-password.component').then(
+            (m) => m.ForgetPasswordComponent
+          ),
+      },
+    ],
+  },
+  {
+    path: 'main',
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./main/main/main.component').then((m) => m.MainComponent),
+      },
+      {
+        path: 'post/:id',
+        loadComponent: () =>
+          import('./main/post-list/post-item/post-item.component').then(
+            (m) => m.PostItemComponent
+          ),
+      },
+    ],
+  },
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];

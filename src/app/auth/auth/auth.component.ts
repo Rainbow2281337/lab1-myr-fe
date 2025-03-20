d41c7dda87de5b001535802ea80e2608d4c7ced1
Oleksii <alexey.shevchenko.03@gmail.com>
2025-03-20 08:29:54 +0200

    Implement authentication and registration forms with validation

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -1,11 +1,55 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { UserService } from '../../services/user/user.service';
 
 @Component({
   selector: 'app-auth',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css'
+  styleUrl: './auth.component.css',
 })
 export class AuthComponent {
+  authForm!: FormGroup;
 
+  constructor(
+    private readonly router: Router,
+    private readonly userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    this.authForm = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+    });
+  }
+
+  loginUser(event: Event) {
+    event.preventDefault();
+
+    if (this.authForm.invalid) {
+      return;
+    }
+    const userData = this.authForm.value;
+
+    this.userService.logInUser(userData).subscribe({
+      next: (response) => {
+        if (response) {
+          this.router.navigate(['/main']);
+        }
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
 }
--- src/app/reg/reg/reg.component.ts
+++ src/app/reg/reg/reg.component.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { UserService } from '../../services/user/user.service';
 
 @Component({
   selector: 'app-reg',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './reg.component.html',
-  styleUrl: './reg.component.css'
+  styleUrl: './reg.component.css',
 })
-export class RegComponent {
+export class RegComponent implements OnInit {
+  regForm!: FormGroup;
 
+  constructor(
+    private readonly router: Router,
+    private readonly userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    this.regForm = new FormGroup({
+      username: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+    });
+  }
+
+  registerUser(event: Event) {
+    event.preventDefault();
+
+    if (this.regForm.invalid) {
+      return;
+    }
+    const userData = this.regForm.value;
+
+    this.userService.regUser(userData).subscribe({
+      next: (response) => {
+        if (response) {
+          this.router.navigate(['/main']);
+        }
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
 }
